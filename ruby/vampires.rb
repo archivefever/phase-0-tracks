2d0044f6b3394448861c6ed1e4d78145077d00b3
Clint Thacker <couscous@gmail.com>
2017-06-21 15:10:37 -0500

    Update with vampire test v1

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,10 +3,33 @@
 puts "Name:"
 name = gets.chomp
 puts "Age:"
-age = gets.chomp
+age = gets.chomp.to_i
 puts "Birth year:"
-birth_year = gets.chomp
+birth_year = gets.chomp.to_i
+true_age = 2017 - birth_year
 puts "Enjoy some delicious garlic bread? (y/n)"
 garlic_bread = gets.chomp
 puts "Enroll in health insurance? (y/n)"
 insurance = gets.chomp
+
+if age != true_age
+  age_test_passed = true
+end
+
+if garlic_bread == "y"
+  garlic_test_passed = true
+end
+
+if insurance == "y"
+  insurance_test_passed = true
+end
+
+if age_test_passed && garlic_test_passed && insurance_test_passed
+  puts "Probably not a vampire."
+elsif !age_test_passed && (!garlic_test_passed || !insurance_test_passed)
+  puts "Probably a vampire."
+elsif name == "Drake Cula" || name == "Tu Fang"
+  puts "Definitely a vampire."
+else
+  puts "Results inconclusive."
+end
