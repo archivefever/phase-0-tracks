f3ab110acfddf4c3314f4dce79a3ea63ff8ccb7b
Clint Thacker <couscous@gmail.com>
2017-06-21 14:18:43 -0500

    Add initial questions, vampire questionnaire

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+#Vampire Detection program
+
+puts "Name:"
+name = gets.chomp
+puts "Age:"
+age = gets.chomp
+puts "Birth year:"
+birth_year = gets.chomp
+puts "Enjoy some delicious garlic bread? (y/n)"
+garlic_bread = gets.chomp
+puts "Enroll in health insurance? (y/n)"
+insurance = gets.chomp
