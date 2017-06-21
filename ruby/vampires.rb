cbc9819567b2976cc3a612a9172e2a417b986928
Clint Thacker <couscous@gmail.com>
2017-06-21 15:49:23 -0500

    Fix conditional logic issues

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -24,14 +24,14 @@ if insurance == "y"
   insurance_test_passed = true
 end
 
-if age_test_passed && garlic_test_passed && insurance_test_passed
+if name == "Drake Cula" || name == "Tu Fang"
+  puts "Definitely a vampire."
+elsif age_test_passed && garlic_test_passed && insurance_test_passed
   puts "Probably not a vampire."
 elsif !age_test_passed && !garlic_test_passed && !insurance_test_passed
   puts "Almost certainly a vampire."
-elsif !age_test_passed && !(garlic_test_passed || insurance_test_passed)
+elsif !age_test_passed && (!garlic_test_passed || !insurance_test_passed)
   puts "Probably a vampire."
-elsif name == "Drake Cula" || name == "Tu Fang"
-  puts "Definitely a vampire."
 else
   puts "Results inconclusive."
 end
