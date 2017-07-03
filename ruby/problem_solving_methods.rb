0376db5f87899f187047d3bbc709942fa7a62b59
Clint Thacker <couscous@gmail.com>
2017-07-03 16:20:14 -0500

    Update with release 2

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -71,3 +71,43 @@ if fib(100).last == 218922995834555169026
 else
   p "Try again, my dude."
 end
+
+=begin
+
+Release 2
+
+# Pseudocode bubble sorting alogrithm
+
+
+
+=end
+
+test = [1, 2, 3, 4, 5, 6, 7, 8]
+shuffle_test = test.shuffle
+def sorter(ary, control)
+  i = 0
+
+  until ary == control do
+
+    if ary[i + 1] == nil
+      p "starting over"
+      i = 0
+
+    elsif ary[i] > ary[i + 1]
+      p "Swap occurred"
+      ary[i], ary[i + 1] = ary[i + 1], ary[i]
+      p ary
+      i += 1
+
+    else
+      p "Swap did not occur"
+      p ary
+      i += 1
+    end
+
+  end
+
+end
+p shuffle_test
+p "^ Shuffled"
+p sorter(shuffle_test, test)
