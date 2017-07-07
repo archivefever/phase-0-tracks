3a8423ad1451a3099881e6d4d416075196b87a56
Clint Thacker <couscous@gmail.com>
2017-07-07 10:24:57 -0400

    Initial commit with release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,23 @@
+=begin
+
+Puppy Class
+
+SPECIES -------------------
+Canis familiaris
+
+CHARACTERISTICS -----------
+Eye size: varies
+Adorability factor: 10
+Name: varies
+Fur color: varies
+Friendly: yes
+
+
+BEHAVIOR ------------------
+Cuddle
+Lick
+Squirm
+Jump
+
+
+=end
