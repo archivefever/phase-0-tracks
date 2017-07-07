18c8028c6e102f0cd68357d58bfb5a0d4f11f936
Clint Thacker <couscous@gmail.com>
2017-07-07 11:38:59 -0400

    Add baker class and instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -21,6 +21,10 @@ class Puppy
     puts "*puppy miraculously speaks*: 'I don't know the #{song}!'"
   end
 
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
+
 end
 
 
@@ -31,3 +35,34 @@ ajax.speak(3)
 ajax.roll_over
 ajax.dog_years(15)
 ajax.dance("Tango")
+
+
+class Baker
+
+  def initialize(baker_number)
+    @name = "Baker ##{baker_number + 1}"
+  end
+
+  def crack_eggs(eggs)
+    eggs.times { puts "*egg cracks*"}
+  end
+
+  def convert_ounces_to_grams(ounces)
+    ounces * 28
+  end
+
+  def weigh_flour
+    puts "*flour is everywhere*"
+  end
+
+end
+
+baker_array = Array.new(50){ |i| Baker.new(i) }
+
+p baker_array
+
+baker_array.each do |baker|
+  baker.crack_eggs(2)
+  baker.convert_ounces_to_grams(3)
+  baker.weigh_flour
+end
