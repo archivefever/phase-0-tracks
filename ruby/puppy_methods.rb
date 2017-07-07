dc58adb1e5d814dc0803f366835bba00a7221fe5
Clint Thacker <couscous@gmail.com>
2017-07-07 10:52:47 -0400

    Add methods to puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,29 @@ class Puppy
     toy
   end
 
+  def speak(command_number)
+    command_number.times { puts "Woof!" }
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    human_years * 7
+  end
+
+  def dance(song)
+    puts "*puppy miraculously speaks*: 'I don't know the #{song}!'"
+  end
+
 end
 
+
+ajax = Puppy.new
+
+ajax.fetch("plutonium")
+ajax.speak(3)
+ajax.roll_over
+ajax.dog_years(15)
+ajax.dance("Tango")
