d776e339aa2926700ba81fb9cae1861b645ba894
Clint Thacker <couscous@gmail.com>
2017-07-08 09:51:32 -0400

    Add release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,6 +1,36 @@
 
 module Shout
 
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yelling_happily(words)
+    words + "! "+ "Oh joy of joys!"
+  end
+
+end
+
+class Auctioneer
+  include Shout
+end
+
+class Coach
+  include Shout
+end
+
+sothebys_guy = Auctioneer.new
+p sothebys_guy.yell_angrily("Is there no other bid")
+p sothebys_guy.yelling_happily("Sold")
+
+coach = Coach.new
+p coach.yell_angrily("Bad call")
+p coach.yelling_happily("Good hustle")
+
+
+=begin
+module Shout
+
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end
@@ -13,3 +43,5 @@ end
 
 p Shout.yell_angrily("fiddlesticks")
 p Shout.yelling_happily("i won")
+
+=end
