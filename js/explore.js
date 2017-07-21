bac8afb71b1508b92162e36f94678cf066205658
Clint Thacker <couscous@gmail.com>
2017-07-21 09:58:51 -0500

    Add reverse algorithm

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+// Reverse Algorithm
+// reverse("hello") => "olleh"
+// Declare an empty string variable
+// Set a counter to (word length - 1)
+// Iterate through the string (as an array)
+// For each index position counting down from (string length - 1) to 0, add the character at that index to the new string
+// Subtract one from the counter
+// Return the new string
+
+
